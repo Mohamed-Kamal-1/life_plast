b02b1abf073ec45b62467282f487b8085b5cdba1
moham <mk22443388@gmail.com>
2026-04-09 19:07:12 +0200

    Set up dependency injection and temporarily disable the stock table widget
    - Added generated dependency injection configuration using `injectable`.
    - Registered `ProductService` as a singleton and `InventoryCubit` as a factory in the service locator.
    - Commented out the `StockTable` widget in `stock_table.dart` due to syntax errors and pending refactoring.

--- lib/core/app_theme/app_theme.dart
+++ lib/core/app_theme/app_theme.dart
@@ -4,8 +4,8 @@ import 'package:flutter/material.dart';
 class AppTheme {
   static ThemeData lightTheme = ThemeData(
     useMaterial3: true,
-    // scaffoldBackgroundColor: const Color(0xffF4F6F8),
-    scaffoldBackgroundColor:  Colors.brown,
+    scaffoldBackgroundColor: const Color(0xffF4F6F8),
+    // scaffoldBackgroundColor:  Colors.brown,
     colorScheme: ColorScheme.fromSeed(
       seedColor: const Color(0xff1E293B),
     ),
